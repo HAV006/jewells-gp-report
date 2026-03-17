09bf7e3e4cf614828204259966d83f1336e5559b
Hector Alvarez Vazquez <coirachas@gmail.com>
2026-03-17 09:00:05 +0100

    Update to display stock ATP information by Sku/Store

--- app.js
+++ app.js
@@ -74,6 +74,7 @@ function normalizeData(payload){
       ISOYear: Number(r.ISOYear ?? r.isoyear ?? 0),
       ISOWeek: Number(r.ISOWeek ?? r.isoweek ?? 0),
       Units: Number(r.Units_NewStore ?? r.units_newstore ?? r.Units ?? r.units ?? 0),
+      ExecutionStockQty: Number(r.ExecutionStockQty ?? r.executionstockqty ?? 0),
       NetSales: Number(r.NetSales ?? r.netsales ?? 0),
       COGS: Number(r.COGS ?? r.cogs ?? 0),
       GrossProfit: Number(r.GrossProfit ?? r.grossprofit ?? 0),
@@ -123,6 +124,7 @@ function getExportRows(){
     ISOYear: r.ISOYear,
     ISOWeek: r.ISOWeek,
     Units: Number(r.Units || 0),
+    StockATP: Number(r.ExecutionStockQty || 0),
     NetSales: Number(r.NetSales || 0),
     COGS: Number(r.COGS || 0),
     GrossProfit: Number(r.GrossProfit || 0),
@@ -390,6 +392,7 @@ function renderTable(){
       <td>${r.ISOYear}</td>
       <td>${r.ISOWeek}</td>
       <td class="num">${fmtNum(r.Units)}</td>
+      <td class="num">${fmtNum(r.ExecutionStockQty)}</td>
       <td class="num">${fmtGBP(r.NetSales)}</td>
       <td class="num">${fmtGBP(r.COGS)}</td>
       <td class="num">${fmtGBP(r.GrossProfit)}</td>
